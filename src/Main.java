8998d2044d6a43b4edbbfeced2e11ed11d6ccbae
laporeon <175569928+laporeon@users.noreply.github.com>
2025-07-07 14:33:38 -0300

    feat: implement menu call and first options

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import application.CommandLineInterface;
+import controller.ExpenseController;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        ExpenseController expenseController = new ExpenseController();
+        CommandLineInterface commandLineInterface = new CommandLineInterface();
+
+        while (!exit) {
+            commandLineInterface.showMenu();
+            System.out.print("Choose an option: ");
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    expenseController.add();
+                    commandLineInterface.pressEnter(scanner);
+                    break;
+                case 2:
+                    expenseController.list();
+                    commandLineInterface.pressEnter(scanner);
+                    break;
+                case 0:
+                    System.out.println("\nExiting...");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again...");
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
