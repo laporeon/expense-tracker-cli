2d8dd50c44f1e8a025faa661b5a0d553f99af759
laporeon <175569928+laporeon@users.noreply.github.com>
2025-07-07 14:31:51 -0300

    feat: create entity for expenses

---
+++ src/entity/Expense.java
@@ -0,0 +1,49 @@
+package entity;
+
+import java.math.BigDecimal;
+
+public class Expense {
+    private int id;
+    private BigDecimal amount;
+    private String description;
+    private String date;
+
+    public Expense(int id, BigDecimal amount, String description, String date) {
+        this.id = id;
+        this.amount = amount;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
