201d4e6ef40da55608f624a5944e487074dd790d
laporeon <175569928+laporeon@users.noreply.github.com>
2025-07-08 14:36:48 -0300

    refactor: create start method

--- src/application/CommandLineInterface.java
+++ src/application/CommandLineInterface.java
@@ -1,11 +1,54 @@
 package application;
 
+import controller.ExpenseController;
 import enums.Colors;
 
 import java.util.Scanner;
 
 public class CommandLineInterface {
-    public void showMenu() {
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+
+        ExpenseController expenseController = new ExpenseController();
+
+        while (!exit) {
+            showMenu();
+            System.out.printf("%sChoose an option: %s", Colors.BOLD, Colors.RESET);
+            int option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    expenseController.add();
+                    pressEnter(scanner);
+                    break;
+                case 2:
+                    expenseController.list();
+                    pressEnter(scanner);
+                    break;
+                case 3:
+                    expenseController.delete();
+                    pressEnter(scanner);
+                    break;
+                case 4:
+                    expenseController.summary();
+                    pressEnter(scanner);
+                    break;
+                case 0:
+                    System.out.println("\nExiting...");
+                    exit = true;
+                    break;
+                default:
+                    System.out.printf("%sInvalid option. Try again...%s", Colors.RED, Colors.RESET);
+                    break;
+            }
+        }
+        scanner.close();
+    }
+
+    private void showMenu() {
         System.out.printf("%s" +
                 "\n" +
                 "================================\n" +
@@ -20,14 +63,14 @@ public class CommandLineInterface {
                 "%s\n", Colors.BLUE, Colors.RESET);
     }
 
-    public void pressEnter(Scanner scanner) {
+    private void pressEnter(Scanner scanner) {
         System.out.printf("\n%sPress enter to continue... ", Colors.BOLD);
         scanner.nextLine();
 
         clean();
     }
 
-    public void clean() {
+    private void clean() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
