3b5d85f1f1326e86d197684adc76268e3f390806
laporeon <175569928+laporeon@users.noreply.github.com>
2025-07-10 10:30:12 -0300

    refactor: call validateInput method for id

--- src/controller/ExpenseController.java
+++ src/controller/ExpenseController.java
@@ -19,11 +19,11 @@ public class ExpenseController {
     public void add() {
         System.out.print("\n\n");
         double amount = (double) validator.validateInput(scanner, ExpenseField.AMOUNT.getPromptMessage(),
-                ExpenseField.AMOUNT.getErrorMessage(), ExpenseField.AMOUNT);
+                ExpenseField.AMOUNT.getErrorMessage(), ExpenseField.AMOUNT, expenses.size());
         String description = (String) validator.validateInput(scanner, ExpenseField.DESCRIPTION.getPromptMessage(),
-                ExpenseField.DESCRIPTION.getErrorMessage(), ExpenseField.DESCRIPTION);
+                ExpenseField.DESCRIPTION.getErrorMessage(), ExpenseField.DESCRIPTION, expenses.size());
         String date = (String) validator.validateInput(scanner, ExpenseField.DATE.getPromptMessage(),
-                ExpenseField.DATE.getErrorMessage(), ExpenseField.DATE);
+                ExpenseField.DATE.getErrorMessage(), ExpenseField.DATE, expenses.size());
         Expense expense = new Expense(expenses.size() + 1, amount, description, date);
         expenses.add(expense);
         System.out.printf("\n%s✓ Expense successfully added! (ID: %d) %s%n", Color.GREEN, expense.getId(), Color.RESET);
@@ -42,23 +42,17 @@ public class ExpenseController {
 
     public void update() {
         System.out.print("\n\n");
-        System.out.print("Enter the ID of the expense you want to update: ");
-        int id = scanner.nextInt();
-        scanner.nextLine();
-
-        if (!validator.isValidId(id, expenses.size())) {
-            System.out.printf("%s❌ Invalid or missing ID.%s\n", Color.RED, Color.RESET);
-            return;
-        }
+        int id = (int) validator.validateInput(scanner, ExpenseField.ID.getPromptMessage(),
+                ExpenseField.ID.getErrorMessage(), ExpenseField.ID, expenses.size());
 
         Expense expense = expenses.get(id - 1);
 
         double newAmount = (double) validator.validateInput(scanner, "Enter new amount: ",
-                ExpenseField.AMOUNT.getErrorMessage(), ExpenseField.AMOUNT);
+                ExpenseField.AMOUNT.getErrorMessage(), ExpenseField.AMOUNT, expenses.size());
         String newDescription = (String) validator.validateInput(scanner, "Enter new description: ",
-                ExpenseField.DESCRIPTION.getErrorMessage(), ExpenseField.DESCRIPTION);
+                ExpenseField.DESCRIPTION.getErrorMessage(), ExpenseField.DESCRIPTION, expenses.size());
         String newDate = (String) validator.validateInput(scanner, "Enter new date (i.e yyyy-MM-dd): ",
-                ExpenseField.DATE.getErrorMessage(), ExpenseField.DATE);
+                ExpenseField.DATE.getErrorMessage(), ExpenseField.DATE, expenses.size());
 
         expense.setAmount(newAmount);
         expense.setDescription(newDescription);
@@ -69,14 +63,8 @@ public class ExpenseController {
 
     public void delete() {
         System.out.print("\n\n");
-        System.out.print("Enter expense ID: ");
-        int id = scanner.nextInt();
-        scanner.nextLine();
-
-        if (!validator.isValidId(id, expenses.size())) {
-            System.out.printf("%s❌ Invalid or missing ID.%s\n", Color.RED, Color.RESET);
-            return;
-        }
+        int id = (int) validator.validateInput(scanner, ExpenseField.ID.getPromptMessage(),
+                ExpenseField.ID.getErrorMessage(), ExpenseField.ID, expenses.size());
 
         expenses.remove(id - 1);
         System.out.printf("\n%s✓ Expense successfully deleted!%s%n", Color.GREEN, Color.RESET);
