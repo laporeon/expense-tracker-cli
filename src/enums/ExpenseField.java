a63128777a538d74402ab256185a891e78e22abc
laporeon <175569928+laporeon@users.noreply.github.com>
2025-07-10 10:28:41 -0300

    feat: add ID to ExpenseField enum and remove unused args

--- src/enums/ExpenseField.java
+++ src/enums/ExpenseField.java
@@ -1,18 +1,17 @@
 package enums;
 
 public enum ExpenseField {
-    AMOUNT("Enter amount: ", "Invalid amount!", Double.class),
-    DESCRIPTION("Enter description: ", "Invalid description! Description cannot be null and must have at least 3 characters", String.class),
-    DATE("Enter date (i.e yyyy-MM-dd): ", "Invalid date format! Please use yyyy-MM-dd (e.g., 2025-07-07)", String.class);
+    ID("Enter amount ID: ", "Invalid or missing id! Try again..."),
+    AMOUNT("Enter amount: ", "Invalid amount!"),
+    DESCRIPTION("Enter description: ", "Invalid description! Description cannot be null and must have at least 3 characters"),
+    DATE("Enter date (i.e yyyy-MM-dd): ", "Invalid date format! Please use yyyy-MM-dd (e.g., 2025-07-07)");
 
     private final String promptMessage;
     private final String errorMessage;
-    private final Class<?> type;
 
-    ExpenseField(String promptMessage, String errorMessage, Class<?> type) {
+    ExpenseField(String promptMessage, String errorMessage) {
         this.promptMessage = promptMessage;
         this.errorMessage = errorMessage;
-        this.type = type;
     }
 
     public String getPromptMessage() {
@@ -23,7 +22,4 @@ public enum ExpenseField {
         return errorMessage;
     }
 
-    public Class<?> getType() {
-        return type;
-    }
 }
